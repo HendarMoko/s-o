054c5a3eb327e0eb8df1a99cc736515a48c10e0e
Hendar Moko <86758223+HendarMoko@users.noreply.github.com>
2022-06-10 13:06:34 +0700

    Initialize ktl-751-community-fixes

---
+++ static/js/ktl-component/header/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GlobalHeader from '@jetbrains/kotlin-web-site-ui/out/components/header';
+
+const Header = (props) => {
+  return (
+      <GlobalHeader { ... props } />
+  );
+}
+
+export default Header;
